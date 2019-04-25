fe1d15b44e6cd0eee18c6d7e497ce81afad5d216
Rich Infante <rich@richinfante.com>
2019-04-25 17:48:46 -0400

    Initial Commit

---
+++ satellite-saver-2/satellite_saver_2View.h
@@ -0,0 +1,13 @@
+//
+//  satellite_saver_2View.h
+//  satellite-saver-2
+//
+//  Created by Rich Infante on 4/25/19.
+//  Copyright © 2019 Rich Infante. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface satellite_saver_2View : ScreenSaverView
+
+@end
---
+++ satellite-saver-2/satellite_saver_2View.m
@@ -0,0 +1,52 @@
+//
+//  satellite_saver_2View.m
+//  satellite-saver-2
+//
+//  Created by Rich Infante on 4/25/19.
+//  Copyright © 2019 Rich Infante. All rights reserved.
+//
+
+#import "satellite_saver_2View.h"
+
+@implementation satellite_saver_2View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
